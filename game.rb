61f1ab69e003245b0ab28bf90d54b091ae2bfe1c
charuzain <charuabhizain@gmail.com>
2022-01-07 22:46:12 -0500

    added loop and player switch functionality

--- game.rb
+++ game.rb
@@ -5,26 +5,33 @@ class Game
 def initialize 
   @player1 = Player.new("player1")
   @player2 = Player.new("player2")
+  @players =[@player1 ,@player2].shuffle
 end
+
+def game_over?
+  @player1.lives <= 0 || @player2.lives <= 0
+end
+
 
 def play
   puts "let's start the game !"
   puts @player1.inspect
   puts @player2.inspect
 
-  num1 = rand(20)+1
-  puts num1
-  num2 = rand(20)+1
-  puts num2
-  sum = num1 + num2
-  puts "#{@player1.name}: What does #{num1} plus #{num2} equal ?"
-  response = gets.chomp
-  if response != sum 
-    puts "#{@player1.name}: Seriously? No !"
-    @player1.lives = @player1.lives-1
-    puts "P1 : #{@player1.lives}/3"
-  end
-
+      until(game_over?) do
+        @players.rotate!
+        current_player = @players.first
+        num1 = rand(20)+1
+        num2 = rand(20)+1
+        sum = num1 + num2
+        puts "#{current_player.name}: What does #{num1} plus #{num2} equal ?"
+        response = gets.chomp.to_i
+        if response != sum 
+          puts "#{current_player.name}: Seriously? No !"
+          current_player.lives = current_player.lives-1
+          puts "P1 : #{@player1.lives}/3  vs P2: #{@player2.lives}/3 "
+        end
+    end
   end
 end
 
